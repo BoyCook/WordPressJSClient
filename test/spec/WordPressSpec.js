84e8467fc15c9fd1ecfa673b304d87507de29e0e
Craig Cook <boycook@me.com>
2013-03-04 13:00:41 +0000

    Removed JSON parse from client

--- test/spec/WordPressSpec.js
+++ test/spec/WordPressSpec.js
@@ -1,40 +1,36 @@
 var WordPress = require('../../lib/wordpress').WordPress;
 
-describe('WordPress', function () {
-    var blog;
-    var config = {
-        consumerKey: 'consumer_key',
-        consumerSecret: 'foobar',
-        accessToken: 'access_token',
-        accessTokenSecret: 'access_token_secret',
-        callBackUrl: 'http://craigcook.co.uk/auth/twitter/callback'
-    };
+describe('WordPress', function() {
+	var blog;
+	var config = {
+		consumerKey: 'consumer_key',
+		consumerSecret: 'foobar',
+		accessToken: 'access_token',
+		accessTokenSecret: 'access_token_secret',
+		callBackUrl: 'http://craigcook.co.uk/auth/twitter/callback'
+	};
 
-    beforeEach(function (done) {
-        blog = new WordPress(config);
-        expect(blog).toBeDefined();
-        expect(blog.oauth).toBeDefined();
-        done();
-    });
+	beforeEach(function(done) {
+		blog = new WordPress(config);
+		expect(blog).toBeDefined();
+		expect(blog.oauth).toBeDefined();
+		done();
+	});
 
-    it('should get posts', function (done) {
-        blog.getPosts('boycook.wordpress.com', undefined, {},
-            function (data) {
-                expect(data.posts.length).toEqual(18);
-                done();
-            }
-        );
-    });
-
-    it('should handle error correctly', function (done) {
-        blog.getPosts('boycookxxxxxxxxx.wordpress.com', undefined, 
-			function(err, response, body) {
-                expect(err.statusCode).toEqual(404);
-		        console.log('ERROR [%s]', JSON.stringify(err));				
-		        if (done) {
-		            done();
-		        }	
-			}, 
-		{});
-    });
+	it('should get posts', function(done) {
+		blog.getPosts('boycook.wordpress.com', undefined, {}, function(data) {
+			data = JSON.parse(data);
+			expect(data.posts.length).toEqual(18);
+			done();
+		});
+	});
+	
+	it('should handle error correctly', function(done) {
+		blog.getPosts('boycookxxxxxxxxx.wordpress.com', undefined, function(err, response, body) {
+			expect(err.statusCode).toEqual(404);
+			if (done) {
+				done();
+			}
+		}, {});
+	});
 });
